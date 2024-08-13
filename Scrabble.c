d32af4c056ecd4a6a6725d9c7382726b955a06e7
GuilhermeAmeixaF <guilhermeameixafreitas@gmail.com>
2024-08-13 16:35:29 -0300

    Implementando array de pontuações e otimizando estrutura do código.

--- Scrabble.c
+++ Scrabble.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
- 
+
+
+  int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10}; // ARRAY DE PONTOS
+
  int main(void)
  {
     /* Jogo proposto onde 2 jogadores escolhem uma palavra ou nome respectivamente e baseado na complexidade
@@ -15,7 +18,5 @@
     printf("Jogador2 : ");
     gets(jogador2);
 
-    char char_pontuacoes = {{'A',1},{'B',2}};
-    printf("Caracter e seu valor\n %c - %i\n\n", char_pontuacoes[0][0], char_pontuacoes[0][1]);
 
  }
