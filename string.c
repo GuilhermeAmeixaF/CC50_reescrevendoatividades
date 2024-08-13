b118e9f4cf1aca8c2f5765847faf7fbf424706f0
GuilhermeAmeixaF <guilhermeameixafreitas@gmail.com>
2024-08-13 10:57:49 -0300

    Implementando código, iniciando string em C - array

--- string.c
+++ string.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
+#include<string.h>
 
 int main(void)
 {
-    
+    char texto[30]; // Array de string
+
+    printf("Digite a string:");
+    scanf("%s", texto);
+
+    printf("Acessando e imprimindo caracter a caracter da string.\n");
+    for(int i = 0, tamanho_string = strlen(texto); i < tamanho_string; i++)
+    {
+        printf("%c\n", texto[i]);
+    }
 }
